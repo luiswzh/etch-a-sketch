8bd4f6e24201691d5deb67da6703cb8ae496244c
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-29 14:54:47 -0500

    Add color and size selection, and reset

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //Initial declarations
-const gridSize = 16;
+let gridSize = 16;
 const blackButton = document.querySelector('#black');
 const customButton = document.querySelector('#custom');
 const rgbButton = document.querySelector('#rgb');
-const darkenButton = document.querySelector('#darken');
+const greyScaleButton = document.querySelector('#grey-scale');
 const gridSizeButton = document.querySelector('#grid-size');
 const resetButton = document.querySelector('#reset');
 const grid = document.querySelector('.grid');
+const colorPalette=document.querySelector('#custom input')
 
 //Options:
+let color = 'black';
+let greyScale = 200;
 
 //Response functions
 function createGrid(size){
@@ -46,9 +49,65 @@ function resizePixel(){
 }
 
 function colorPixel(){
+    switch(color){
+        case 'grey-scale':
+            if (greyScale<0){greyScale = 200}
+            let x = greyScale;
+            this.style.backgroundColor='rgb('+x+','+x+','+x+')';
+            greyScale += -20;
+            break;
 
+        case 'rgb':
+            this.style.backgroundColor=randomColor();
+            break;
+
+        default:
+            this.style.backgroundColor=color;
+    }
 }
+
+function randomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+//Event listeners
+
+customButton.addEventListener('click', ()=>{
+    color = colorPalette.value;
+});
+
+colorPalette.addEventListener('change', ()=>{
+    color = colorPalette.value;
+});
+
+blackButton.addEventListener('click',()=>{
+    color = '#000000';
+});
+
+rgbButton.addEventListener('click', ()=>{
+    color = 'rgb';
+});
+
+greyScaleButton.addEventListener('click', ()=>{
+    color = 'grey-scale';
+});
+
+gridSizeButton.addEventListener('click', ()=>{
+    clearGrid();
+    gridSize=prompt('Input grid size: ');
+    createGrid(gridSize);
+});
+
+resetButton.addEventListener('click',()=>{
+    clearGrid();
+    createGrid(gridSize);
+});
 
 //Initial script
 window.addEventListener('resize',resizePixel);
-
+createGrid(gridSize);
